bf6def4385ff3badc17df882efc5e226ccff7b78
raman <raman@sveltetech.com>
2022-11-23 23:22:34 +0530

    array method

---
+++ Array.js
@@ -0,0 +1,7 @@
+let arr = [1, 2, 3, 4, 5];
+arr.push(0); //add in end
+arr.unshift(1); //add in starting
+arr.shift(); //remove first element in array
+arr.pop(); //remove from last
+
+console.log(arr);
